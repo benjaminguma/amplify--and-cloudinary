abb9858d4c4dcdeda85d95733cd9a0f42988c8c5
Benjamin Guma <benjaminguma.bg@gmail.com>
2022-01-05 01:01:24 +0100

    some change

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "myamplifyapp123e4734f7c": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "storage": {
+        "mystorage": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
